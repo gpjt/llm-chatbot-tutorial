99794293e28abf5f64b3cb3d5dac0183cf5b787f
Giles Thomas <giles@giles.net>
2023-04-04 21:00:13 +0100

    Failed attempt #1 -- using GUIDs as 'names' in the transcript

--- part-3/bot.py
+++ part-3/bot.py
@@ -1,4 +1,3 @@
-import sys
 from textwrap import dedent
 
 import openai
@@ -22,13 +21,23 @@ def get_completion(model, prompt, temperature, max_tokens, stop):
     return result
 
 
+USER_INTRO = "051db2a5-b725-4b14-86c3-bce9f207834f"
+BOT_INTRO = "4ce7886e-1500-4f45-b5be-3253c75872aa"
+
+
 class Message:
     def __init__(self, sender, content):
         self.sender = sender
         self.content = content
 
     def __str__(self):
-        return f"{self.sender}:\n{self.content}"
+        if self.sender == "User":
+            intro = USER_INTRO
+        elif self.sender == "Bot":
+            intro = BOT_INTRO
+        else:
+            raise Exception(f"Unknown sender {self.sender}")
+        return f"{intro}:\n{self.content}"
 
 
 conversation_history = []
@@ -37,15 +46,22 @@ conversation_history = []
 def generate_response(user_message_content):
     conversation_history.append(Message("User", user_message_content))
 
-    prompt = 'The following is the transcript of a chat between "Bot", a chatbot, and "User", a human using it.\n\n'
+    prompt = dedent(f"""
+        The following is the transcript of a chat between a chatbot and a human using it.
+
+        The user's messages start with the following text: {USER_INTRO}
+
+        The bot's messages start with the following text: {BOT_INTRO}
+
+    """)
     for message in conversation_history:
         prompt += str(message) + "\n\n"
 
-    prompt += "Bot:\n"
+    prompt += f"{BOT_INTRO}:\n"
 
     bot_message_content = get_completion(
         model="text-davinci-003", prompt=prompt,
-        temperature=0.5, max_tokens=30, stop="User:"
+        temperature=0.5, max_tokens=30, stop=USER_INTRO
     )
     bot_message = Message("Bot", bot_message_content)
     conversation_history.append(bot_message)
